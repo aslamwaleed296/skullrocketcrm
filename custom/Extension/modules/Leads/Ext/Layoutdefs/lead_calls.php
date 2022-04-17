37e9d55d0e544002913e1ed2b3948cdeefe33e24
waleed786 <aslamwaleed296@gmail.com>
2022-04-17 21:19:28 +0500

    Calls subpanel under lead | gitignore changed

---
+++ custom/Extension/modules/Leads/Ext/Layoutdefs/lead_calls.php
@@ -0,0 +1,23 @@
+<?php
+
+$layout_defs["Leads"]["subpanel_setup"]['oldcalls'] = array (
+  'order' => 30,
+  'module' => 'Calls',
+  'subpanel_name' => 'default',
+  'sort_order' => 'asc',
+  'sort_by' => 'id',
+  'title_key' => 'LBL_CALLS',
+  'get_subpanel_data' => 'oldcalls',
+  'top_buttons' => 
+  array (
+    0 => 
+    array (
+      'widget_class' => 'SubPanelTopButtonQuickCreate',
+    ),
+    1 => 
+    array (
+      'widget_class' => 'SubPanelTopSelectButton',
+      'mode' => 'MultiSelect',
+    ),
+  ),
+);
